3893f0b8e7169fc5187f983b6e1359a6901dbb19
Isaiah Williams <isaiah@Isaiahs-MacBook-Pro-5.local>
2020-10-03 17:58:15 -0400

    "Problem 5 Completed by Isaiah Oct 3"

--- src/main/java/cd/get/ready/algorithms/EuclidsAlgorithm.java
+++ src/main/java/cd/get/ready/algorithms/EuclidsAlgorithm.java
@@ -2,8 +2,15 @@ package cd.get.ready.algorithms;
 
 public class EuclidsAlgorithm {
 
-    public int gcd(int a, int b){
-        return -1;
-    }
+    public int gcd(int a, int b) {
 
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        } else {
+            int newB = a % b;
+            return gcd(b, newB);
+        }
+    }
 }
